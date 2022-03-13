b59dbc3fd374e29f8c1c50fe3a22c0c7256e7779
Antonio Javier Sánchez Casares <asmegabyte101@gmail.com>
2022-03-13 12:55:42 +0100

    Added group detection for error solving

--- src/app/components/game-grid/game-grid.component.ts
+++ src/app/components/game-grid/game-grid.component.ts
@@ -1,5 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { last, Subscription } from 'rxjs';
 import { TileEvent } from 'src/app/events/tile.event';
 import { PuzzleModel } from 'src/app/models/puzzle.model';
 import { EventManager } from 'src/app/services/event-manager.service';
@@ -18,6 +19,8 @@ export class GameGridComponent implements OnInit {
   private eventSubscription: Subscription[] = [];
   public game: TileState[] = [];
 
+  public rowIndicators: number[][] = [];
+
   constructor(private eventManager: EventManager) { }
 
   ngOnInit() {
@@ -27,6 +30,9 @@ export class GameGridComponent implements OnInit {
     this.eventSubscription.push(this.eventManager.subscribe('changeGridTileState', (tileEvent: TileEvent) => {
 
       this.game[this.puzzleModel.width * tileEvent.positionX + tileEvent.positionY] = tileEvent.fromState;
+
+      this.checkRowIndicator(tileEvent.positionX);
+      this.checkColIndicator(tileEvent.positionY);
 
       this.checkGame();
 
@@ -39,12 +45,11 @@ export class GameGridComponent implements OnInit {
   }
 
   gameInit() {
-    this.game = this.puzzleModel.imageData.map((item) => {
-      return TileState.Empty;
-    });
+    this.game = new Array(this.puzzleModel.imageData.length).fill(TileState.Empty);
+    this.rowIndicators = new Array(this.puzzleModel.rows.length).fill([]);
   }
 
-  checkGame() {
+  checkGame(): void {
     for (let i = 0; i < this.game.length; i++) {
       if (this.puzzleModel.imageData[i] != this.game[i]) {
 
@@ -58,4 +63,61 @@ export class GameGridComponent implements OnInit {
     this.eventManager.broadcast('endGame');
   }
 
+
+  private checkRowIndicator(row: number) {
+    let groups = this.getRowGroups(row);
+
+  }
+
+  private checkColIndicator(col: number) {
+    let groups = this.getColGroups(col);
+
+  }
+
+  private getRowGroups(x: number): number[] {
+
+    let row = this.game.slice(this.puzzleModel.width * x, this.puzzleModel.width * x + 10);
+    return this.arrayGroup(row);
+  }
+
+  private getColGroups(y: number): number[] {
+
+    let col: number[] = [];
+
+    for (let i = 0; i < this.puzzleModel.height; i++) {
+      col.push(this.game[this.puzzleModel.height * i + y]);
+    }
+
+    this.game.slice(this.puzzleModel.width * y, this.puzzleModel.width * y + 10);
+    return this.arrayGroup(col);
+  }
+
+  private arrayGroup(array: number[]): number[] {
+    let groups: number[] = [];
+    let currentValue = -1;
+    let lastValue = -1;
+    let acc = 0;
+
+
+    array.forEach(item => {
+
+      lastValue = currentValue;
+      currentValue = item;
+
+      if (currentValue == 1) {
+        acc++;
+        return;
+      }
+
+      // end group
+      if (lastValue == 1) {
+        if (lastValue != currentValue) {
+          groups.push(acc);
+          acc = 0;
+        }
+      }
+    });
+
+    return groups;
+  }
 }
